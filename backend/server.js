d5eb164f1a6716410cee18d03e971458486ccba5
GLORY-JAIN <glory.jain@kalvium.community>
2025-02-28 12:42:44 +0530

    connected mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./config/db");
 
 
 const app = express();
@@ -8,11 +9,11 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB Atlas
-
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Welcome to Paw-sonality Match");
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server running on port http://localhost:${PORT}`));
